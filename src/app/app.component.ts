761627ad9abc6b59384ec884f309987644071f43
Soufiene OUERTANI <soufiene.ouertani@gmail.com>
2024-02-07 21:35:35 +0100

    104. Understanding ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   numbers = [1, 2, 3, 4, 5];
   onlyOdd = false;
+  num : number;
+  id: NodeJS.Timeout;
+
+  ngOnInit() {
+    this.id = setInterval(() => {this.battleInit();}, 1000);
+  }
+  battleInit() {
+    this.num = Math.round((((Math.random() * 100) / 10)));
+    console.log(this.num);
+  }
+  
+  ngOnDestroy() {
+    if (this.id) {
+      clearInterval(this.id);
+    }
+  }
 }
